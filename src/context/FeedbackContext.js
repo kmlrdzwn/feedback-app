8f59cb84f0f94d7c99126e5c612f6e9b5713ccb7
kmlrdzwn <kamal.redzwan@gmail.com>
2022-06-12 20:44:50 +0800

    Update Feedback Function

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -40,6 +40,13 @@ export const FeedbackProvider = ({ children }) => {
     setFeedback([newFeedback, ...feedback])
   }
 
+  // Update feedback item
+  const updateFeedback = (id, uptItem) => {
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...uptItem } : item))
+    )
+  }
+
   // Set item to be updated
   const editFeedback = (item) => {
     setFeedbackEdit({
@@ -52,10 +59,11 @@ export const FeedbackProvider = ({ children }) => {
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
         deleteFeedback,
         addFeedback,
         editFeedback,
-        feedbackEdit,
+        updateFeedback,
       }}
     >
       {children}
